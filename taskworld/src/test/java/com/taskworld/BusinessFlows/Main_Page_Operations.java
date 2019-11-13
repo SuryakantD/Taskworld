029a4768cdb2c82e9fe32c70fa59b2fddee1f62e
Dubey, Suryakant <surya.kt90@gmail.com>
2019-11-13 11:52:02 +0530

    added missing file

--- taskworld/src/test/java/com/taskworld/BusinessFlows/Main_Page_Operations.java
+++ taskworld/src/test/java/com/taskworld/BusinessFlows/Main_Page_Operations.java
@@ -2,6 +2,7 @@ package com.taskworld.BusinessFlows;
 
 import org.openqa.selenium.By;
 import com.taskworld.Locators.Main_Page_Locators;
+import com.taskworld.Utilities.Constants;
 import com.taskworld.Utilities.Driver_Setup;
 
 
@@ -11,8 +12,8 @@ public class Main_Page_Operations extends Driver_Setup{
 	public void createNewProject() throws InterruptedException {
 				
 		driver.findElement(By.xpath(Main_Page_Locators.new_project())).click();
-		driver.findElement(By.name(Main_Page_Locators.project_name_input_field())).sendKeys("The sample project");
-		driver.findElement(By.xpath(Main_Page_Locators.project_description_input_field())).sendKeys("The sample project description");
+		driver.findElement(By.name(Main_Page_Locators.project_name_input_field())).sendKeys(Constants.projectName);
+		driver.findElement(By.xpath(Main_Page_Locators.project_description_input_field())).sendKeys(Constants.projectDescription);
 		driver.findElement(By.xpath(Main_Page_Locators.project_next_button())).click();
 		Thread.sleep(2000);
 		driver.findElement(By.xpath(Main_Page_Locators.project_create_button())).click();
@@ -22,7 +23,7 @@ public class Main_Page_Operations extends Driver_Setup{
 	public void createNewTasklist() throws InterruptedException {
 		
 		//driver.findElement(By.xpath(Main_Page_Locators.select_new_project())).click();
-		driver.findElement(By.xpath(Main_Page_Locators.enter_tasklist_name())).sendKeys("This is a TaskList");	// add task list name	and enter
+		driver.findElement(By.xpath(Main_Page_Locators.enter_tasklist_name())).sendKeys(Constants.tasklistName);	// add task list name	and enter
 		driver.findElement(By.xpath(Main_Page_Locators.save_tasklist_name())).click();
 		Thread.sleep(2000);
 	}
@@ -31,7 +32,7 @@ public class Main_Page_Operations extends Driver_Setup{
 		
 		//driver.findElement(By.xpath(Main_Page_Locators.select_new_project())).click();  // Click on project from dashboard
 		driver.findElement(By.xpath(Main_Page_Locators.tasklist_expand())).click(); // click on + icon for TaskList
-		driver.findElement(By.xpath(Main_Page_Locators.add_task_name_input_field())).sendKeys("This is Task");
+		driver.findElement(By.xpath(Main_Page_Locators.add_task_name_input_field())).sendKeys(Constants.taskName);
 		driver.findElement(By.xpath(Main_Page_Locators.tasklist_create_button())).click(); // click on Create button
 		driver.findElement(By.xpath(Main_Page_Locators.expand_open_task_details())).click(); // open task details page
 		driver.findElement(By.xpath(Main_Page_Locators.close_open_task_details())).click(); // close open task details page
--- taskworld/src/test/java/com/taskworld/Utilities/Constants.java
+++ taskworld/src/test/java/com/taskworld/Utilities/Constants.java
@@ -4,7 +4,10 @@ public class Constants {
 	public static final String driverPath="src/test/resources/Drivers/";
 	public static final String username="surya.kt90@gmail.com";
 	public static final String password="test@12345";
-
 	
+	public static final String projectName="The sample project";
+	public static final String projectDescription="The sample project description";
+	public static final String tasklistName="This is a TaskList";
+	public static final String taskName="This is Task";	
 
 }
